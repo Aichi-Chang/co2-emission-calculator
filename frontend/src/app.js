e06ca708d747190d56c7bcc03f9b0adfd06b6090
Aichi Chang <phoebexxxc@gmail.com>
2020-04-04 21:59:46 +0100

    add localstorage authentication

---
+++ frontend/src/app.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './css/index.css'
+import 'tachyons'
+
+
+
+const App = () => {
+
+  return <div>
+
+    <div>HEYYYYYY</div>
+
+  </div>
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
