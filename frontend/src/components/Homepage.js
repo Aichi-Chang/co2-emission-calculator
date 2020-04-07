f8975053f8ba58632a09037b3c692db0a5c163e1
Aichi Chang <phoebexxxc@gmail.com>
2020-04-07 23:07:21 +0100

    tfl api setup

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -14,7 +14,59 @@ export default function Homepage() {
   }, [])
 
 
-  console.log(route)
+  if (!route) {
+    return null
+  }
+
+  const legs = route.journeys.map(summary =>{
+    return summary.legs
+  })
+  // tube
+  const modeObjZero = legs[0].map(mode => {
+    return mode.mode
+  })
+  // tube
+  const modeObjOne = legs[1].map(mode => {
+    return mode.mode
+  })
+  // tube
+  const modeObjTwo = legs[2].map(mode => {
+    return mode.mode
+  })
+  // bus
+  const modeObjThree = legs[3].map(mode => {
+    return mode.mode
+  })
+  // cycle
+  const modeObjFour = legs[4].map(mode => {
+    return mode.mode
+  })
+  // cycle hire
+  const modeObjFive = legs[5].map(mode => {
+    return mode.mode
+  })
+
+
+  console.log(route.journeys)
+  console.log(modeObjZero.map(id => {
+    return id.id
+  }))
+  console.log(modeObjOne.map(id => {
+    return id.id
+  }))
+  console.log(modeObjTwo.map(id => {
+    return id.id
+  }))
+  console.log(modeObjThree.map(id => {
+    return id.id
+  }))
+  console.log(modeObjFour.map(id => {
+    return id.id
+  }))
+  console.log(modeObjFive.map(id => {
+    return id.id
+  }))
+  
 
   return (
     <div>
