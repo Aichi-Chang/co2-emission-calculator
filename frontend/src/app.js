2cdc31759612bb88bbc4c5f8f2428136eabfba31
Aichi Chang <phoebexxxc@gmail.com>
2020-04-28 16:14:11 +0100

    add logout

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter, Switch, Route } from 'react-router-dom'
+import { Link } from 'react-dom'
 
 import './css/index.css'
 import 'tachyons'
 
+import Auth from './lib/Auth'
 
 import Register from './components/Register'
 import Login from './components/Login'
 import Postcodes from './components/Postcodes'
 import User from './components/User'
+import Nav from './components/Nav'
 
 
 const App = () => {
 
+
   return <div>
 
-    {/* logout */}
-
     <HashRouter>
+      <Nav />
 
       <Switch>
         <Route exact path = '/' component={Postcodes} />
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Auth from '../lib/Auth'
+
+export default function Nav() {
+
+
+  function handleLogout() {
+    Auth.logOut()
+    // this.props.history.push('/')
+  }
+
+  return (
+    <div>
+      {Auth.isAuthenticated() && <button onClick={() => handleLogout()}>Log Out</button>}
+      {!Auth.isAuthenticated() && <Link to='/login'>Log in</Link>}
+      {!Auth.isAuthenticated() && <Link to='/register'>Sign Up</Link>}
+
+
+    </div>
+  )
+}
